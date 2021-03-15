56768f2c359b3529a5c71b3d354c5094ab28a1fc
heaven-dev <kah@live.fi>
2021-03-15 08:28:47 +0200

    Version number fixed to locale.js file.

--- js/util/locale.js
+++ js/util/locale.js
@@ -13,7 +13,7 @@ var appName = 'Taivas TV7';
 /**
  * Updated by gulp script during a build of the app.
  */
-var appVersion = '2.0.9';
+var appVersion = '2.0.10';
 
 var logoTaivas = 'images/logo_taivas.png';
 var logoTaevas = 'images/logo_taevas.png';
